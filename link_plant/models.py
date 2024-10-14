1f55879513cbc6fda0562b4c519610e1e261423c
alokshandilya <alok.shandilya10@gmail.com>
2024-10-15 04:24:17 +0530

    fix(models): add Profile and Link models
    - Add Profile and Link models to the link_plant app
    - Create a migration for the new models
    - Register the models with the admin site
    - Add a string representation for the models

--- link_plant/models.py
+++ link_plant/models.py
@@ -2,3 +2,29 @@ from django.db import models
 
 
 # Create your models here.
+class Profile(models.Model):
+    BG_CHOICES = (
+        ("blue", "Blue"),
+        ("green", "Green"),
+        ("yellow", "Yellow"),
+    )
+
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    bg_color = models.CharField(max_length=50, choices=BG_CHOICES)
+
+    def __str__(self):
+        return self.name
+
+
+class Link(models.Model):
+    text = models.CharField(max_length=100)
+    url = models.URLField()
+    profile = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name="links",  # name of reverse relation from Profile to Link
+    )
+
+    def __str__(self):
+        return f"{self.text} | {self.url}"
